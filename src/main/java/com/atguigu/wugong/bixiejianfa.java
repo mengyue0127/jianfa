511ff47421446b54e91111bc83a85d1f0a96f1aa
yuebuqun3333 <yuebuqun3333@sina.com>
2022-06-20 19:52:25 +0800

    辟邪剑法

---
+++ src/main/java/com/atguigu/wugong/bixiejianfa.java
@@ -0,0 +1,10 @@
+package com.atguigu.wugong;
+
+public class bixiejianfa {
+    public static void main(String[] args) {
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+    }
+}
